81bb791d939768cc81226d15b445ba92db9bc705
Elen8 <90869008+Elen8@users.noreply.github.com>
2022-10-24 15:22:11 +0400

    Update Console.h

--- PowerPoint/Console.h
+++ PowerPoint/Console.h
@@ -6,13 +6,41 @@ class Input
 {
 public:
 	void get_input();
-	void parser(std::string&);
 	void get_output();
-	void parser(std::string&);
+	//void parser(std::string&);
 
 private:
 	std::string input;
 	std::string output;
+};
+
+void Input::get_input()
+{
+	std::string input;
+	std::cin >> input;
+}
+
+void Input::get_output()
+{
+	//if token is ready, give it
+}
+
+//void Input::parser(std::string& input)
+//{
+//	
+//}
+
+class Parser
+{
+private:
+	std::string prs;
+public:
+	Parser(std::string& str) :
+		prs(str) {}
+
+	void parse_command();
+	void get_token(std::string&);
+	std::string get_lexeme(const std::string&);
 };
 
 #endif 
