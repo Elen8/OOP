189652ba6b08487d1587bdc44e9fcba26a45aeeb
Elen8 <90869008+Elen8@users.noreply.github.com>
2022-12-05 20:01:27 +0400

    Create Action.h

---
+++ PowerPoint/Action.h
@@ -0,0 +1,14 @@
+#ifndef ACTION_H
+#define ACTION_H
+#include "Slide.h"
+#include "Item.h"
+class CAction
+{
+public:
+	//Functional to Slide and Item
+private:
+	std::shared_ptr<CSlide> PSlide;
+	std::shared_ptr<CItem> PSlide;
+};
+
+#endif //ACTION_H
