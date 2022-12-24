06970efb862213695375ac046281fe717ac50673
Elen8 <90869008+Elen8@users.noreply.github.com>
2022-12-24 09:59:20 +0400

    Update Slide.h

--- PowerPoint/Slide.h
+++ PowerPoint/Slide.h
@@ -4,8 +4,14 @@
 
 class CSlide
 {
+public:
+	CSlide NewSlide();
+	void DeleteSlide(int);
+	void GotoSlide(int);
+
 protected:
 	std::shared_ptr<IItemCollection> IC;
+	int SlideID;
 };
 
 #endif //SLIDE_H
