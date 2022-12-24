d7d11c5b936e44640a0250b4f4a1091adcbb2d69
Elen8 <90869008+Elen8@users.noreply.github.com>
2022-12-24 09:53:05 +0400

    Update Application.h

--- PowerPoint/Application.h
+++ PowerPoint/Application.h
@@ -1,18 +1,25 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
-#include "Document.h"
-#include "Editor.h"
+//#include "Editor.h"
+#include "CommandHandler.h"
 
 class CApplication
 {
 public:
 	static CApplication* Instance();
-
+	void CallCmdHandler()
+	{
+		PCmd = std::make_unique<CCommandHandler>();
+		PCmd->Fetch();
+		PCmd->Decode();
+		PCmd->Do();
+	}
 private:
-	CApplication();
+	CApplication() = default;
 	static CApplication* PApp;
 	std::shared_ptr<CEditor> PEdit;
 	std::unique_ptr<CDocument> PDoc;
+	std::unique_ptr<ICommandHandler> PCmd;
 };
 
 
