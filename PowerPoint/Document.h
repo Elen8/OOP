9851e6f298cedd874a3efb0f19c1d20193e04280
Elen8 <90869008+Elen8@users.noreply.github.com>
2022-12-24 09:55:00 +0400

    Update Document.h

--- PowerPoint/Document.h
+++ PowerPoint/Document.h
@@ -1,12 +1,17 @@
 #ifndef DOCUMENT_H
 #define DOCUMENT_H
 #include "Presentation.h"
+#include "Action.h"
 
 class CDocument
 {
-	//CDocument();
+public:
+	CDocument()
+	{
+		PPres = std::make_unique<CPresentation>();
+	}
 	void SlideShow();
-
+	void Do(std::shared_ptr<Action>(DAct));
 private:
 	std::unique_ptr<CPresentation> PPres;
 };
